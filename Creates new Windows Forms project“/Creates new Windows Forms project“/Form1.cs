77e64d8a7ba97f250de16d09825cbbb63ba36959
Korisnik <Korisnik@DESKTOP-MR1Q4VU>
2023-05-02 22:30:25 +0200

    Creates new Windows Forms project

---
+++ Creates new Windows Forms project“/Creates new Windows Forms project“/Form1.cs
@@ -0,0 +1,10 @@
+namespace Creates_new_Windows_Forms_project__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
